a4d16523d3d76d14f5c14766d551d63dd7b0aab8
Kelang <Kelang@users.noreply.github.com>
2019-02-18 21:29:29 -0500

    Ajout des differents fichiers au dossier modules pour la creation dynamique de la description des projets

---
+++ modules/application.js
@@ -0,0 +1,45 @@
+import {descriptionProjet1} from './descriptionProjet';
+import {descriptionProjet2} from './descriptionProjet';
+import {descriptionProjet3} from './descriptionProjet';
+
+
+
+/*éléments de la page qui contiendront les descriptions des projets */
+let descProj1 = document.getElementById('#descriptionProjet1');
+let descProj2 = document.getElementById('#descriptionProjet2');
+let descProj3 = document.getElementById('#descriptionProjet3');
+
+
+function ajoutDescription() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
